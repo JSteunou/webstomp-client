86d1e18db4dc21adc3e4729f9d5e21d58b411539
yuye <yeyu456@gmail.com>
2016-06-07 13:36:56 +0800

    firefox

--- karma.conf.js
+++ karma.conf.js
@@ -94,7 +94,7 @@ module.exports = function (config) {
         autoWatch: true,
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: ['Firefox'],
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: false,
